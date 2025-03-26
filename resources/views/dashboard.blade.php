c1c252524c3b070c96a4bfa11e0937bc7a7f4eea
LuisDani <spartandanii117@gmail.com>
2025-03-26 13:19:36 -0600

    Cambios

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,9 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+    <p>
+        HOLAAAAAAAA
+    </p>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
